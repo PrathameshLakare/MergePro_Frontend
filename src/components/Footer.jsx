2d7c8a46a2a1feb21e6d6b745bb117ff12249110
Prathamesh Lakare <prathameshlakare001@gmail.com>
2025-05-17 12:11:33 +0530

    feat: create footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className="bg-dark bg-subtle p-2">
+      <p className="container my-1 text-light text-center">&copy; MergePro</p>
+    </div>
+  );
+};
+
+export default Footer;

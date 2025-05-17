4294a13d2574c7205fdd65cc6072b34ec2f0bf55
Prathamesh Lakare <prathameshlakare001@gmail.com>
2025-05-17 12:11:16 +0530

    feat: add redux store

--- src/App/store.js
+++ src/App/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
+import profileSlice from "../features/profile/profileSlice";
 import authSlice from "../features/auth/authSlice";
+import userSlice from "../features/user/userSlice";
 
 export default configureStore({
   reducer: {
     auth: authSlice,
+    profile: profileSlice,
+    users: userSlice,
   },
 });

8405701926390f719f6758b33d830ea1b01dca4d
Prathamesh Lakare <prathameshlakare001@gmail.com>
2025-05-17 12:10:43 +0530

    feat: add router

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.js";
 import { Provider } from "react-redux";
 import store from "./App/store.js";
+import { BrowserRouter as Router } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <Router>
+        <App />
+      </Router>
     </Provider>
   </StrictMode>
 );
